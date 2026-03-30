d7a1ad7afffa896bbe2c4008d19f9eb7970f4ffe
RickHarrod <harrod1@hotmail.com>
2026-03-30 20:14:31 +0100

    Add files via upload

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -65,7 +65,7 @@ export default function About() {
 
             {/* Founder Photo */}
             <img
-              src="https://raw.githubusercontent.com/RickHarrod/harroddiagnostics/refs/heads/main/Rick%20Harrod%20Diagnostics.jpg"
+              src="https://raw.githubusercontent.com/RickHarrod/harroddiagnostics/refs/heads/main/Rick%20PNG%20Harrod%20Diagnostics.png"
               alt="Ricky Harrod"
               className="w-56 md:w-64 lg:w-72 rounded-xl shadow-xl object-cover border border-white/20 dark:border-slate-800/40"
             />
